974e1d2c0f290419efe6ab9a03610b3eb53c0759
Helder Salvalaio <helder.hs@gmail.com>
2022-12-11 17:32:25 -0300

    ae

--- teste.js
+++ teste.js
@@ -6,9 +6,6 @@ const consumer = async () => {
   //await server.publishInQueue('vendas', '{"carro": "car1", "carro2":"car2"}');
   await server.receiver('vendas');
 };
-<<<<<<< HEAD
-//rodar api
-=======
+
 //rodar api 2
->>>>>>> 0f3a8b30b6cd759514da1e2b15564138758b87ac
 consumer();
